183350c5bddd04045058a33233e94448114d18f6
Dmitry <46893211+B1ckbeard@users.noreply.github.com>
2025-10-03 14:55:56 +0300

    add fsd

---
+++ src/pages/main/ui/Page.tsx
@@ -0,0 +1,14 @@
+import styles from "./styles.module.css";
+import LatestNews from "./LatestNews/LatestNews";
+import NewsByFilters from "./NewsByFilters/NewsByFilters";
+
+const MainPage = () => {
+  return (
+    <main className={styles.main}>
+      <LatestNews />
+      <NewsByFilters />
+    </main>
+  );
+};
+
+export default MainPage;
